c652a193b3327dfa52731c3b2c3333c9ba83dd23
Sujan Tamang <everestgaming3@gmail.com>
2023-11-04 12:54:16 +0545

    feat: finish form validation

--- app/page.tsx
+++ app/page.tsx
@@ -43,15 +43,16 @@ export default function Home() {
     onCompleted: ({ search: data }) => {
       const validation = handleAPIValidation(data);
 
-      setFormState(initialFormState);
-      setAlertState({
-        show: true,
-        variant: validation.type,
-        message: validation.message,
-      });
-      setTimeout(() => {
-        setAlertState(initialAlertStateValue);
-      }, 3000);
+      if (validation) {
+        const { show, type, message } = validation;
+        if (type === 'success') {
+          setFormState(initialFormState);
+        }
+        showAlert(show, type, message);
+      }
+    },
+    onError: () => {
+      showAlert(true, 'error', 'Something went wrong.');
     },
   });
 
@@ -67,6 +68,17 @@ export default function Home() {
     }));
   };
 
+  const showAlert = (show: boolean, variant: string, message: string) => {
+    setAlertState({
+      show,
+      variant,
+      message,
+    });
+    setTimeout(() => {
+      setAlertState(initialAlertStateValue);
+    }, 3000);
+  };
+
   const handleLocalValidation = () => {
     let isValid = true;
     const value = { error: true, errorMsg: 'This field is required' };
@@ -79,48 +91,86 @@ export default function Home() {
       handleFieldChange(FormFields.Suburb, value);
       isValid = false;
     }
-    if (!formState[FormFields.State].value) {
-      handleFieldChange(FormFields.State, value);
-      isValid = false;
-    }
 
     return isValid;
   };
 
   const handleAPIValidation = (data: SearchLocalitiesData[] | null) => {
-    const validation = { type: '', message: '' };
+    const validation = { show: false, type: '', message: '' };
 
     const postcode = +formState[FormFields.Postcode].value;
-    const suburb = formState[FormFields.Suburb].value.toLowerCase();
-    const state = formState[FormFields.State].value.toLowerCase();
+    const suburb = formState[FormFields.Suburb].value;
+    const state = formState[FormFields.State].value;
 
-    if (!data || !data.length) {
-      validation.type = 'error';
-      validation.message = 'No fields match.';
+    // Check if all fields matched or not
+    if (postcode && suburb && state) {
+      const isAllFieldsValid = data?.some((locality) => {
+        if (
+          locality.postcode === postcode &&
+          locality.location.toLowerCase() === suburb.toLowerCase() &&
+          locality.state.toLowerCase() === state.toLowerCase()
+        ) {
+          return true;
+        }
+      });
+      if (isAllFieldsValid) {
+        validation.show = true;
+        validation.type = 'success';
+        validation.message = 'The postcode, state and suburb input are valid.';
 
-      return validation;
+        return validation;
+      }
     }
 
-    const isAllFieldsValid = data?.some((locality) => {
-      if (
-        locality.postcode === postcode &&
-        locality.location.toLowerCase() === suburb &&
-        locality.state.toLowerCase() === state
-      ) {
+    // Check combination of form field values
+    if (state) {
+      // Check for suburb & state
+      const isSuburbAndStateValid = data?.some((locality) => {
+        if (
+          locality.location.toLowerCase() === suburb.toLowerCase() &&
+          locality.state.toLowerCase() === state.toLowerCase()
+        ) {
+          return true;
+        }
+      });
+      if (!isSuburbAndStateValid) {
+        handleFieldChange(FormFields.Suburb, {
+          error: true,
+          errorMsg: `The suburb ${suburb} does not exist in the state ${state}.`,
+        });
+        return null;
+      }
+    }
+
+    const isSuburbAndPostcodeValid = data?.some((locality) => {
+      if (locality.postcode === postcode && locality.location.toLowerCase() === suburb.toLowerCase()) {
         return true;
       }
     });
-
-    if (isAllFieldsValid) {
-      validation.type = 'success';
-      validation.message = 'The postcode, state and suburb input are valid.';
+    if (!isSuburbAndPostcodeValid) {
+      // Check for suburb & postcode
+      handleFieldChange(FormFields.Postcode, {
+        error: true,
+        errorMsg: `The postcode ${postcode} does not match the suburb ${suburb}.`,
+      });
+      return null;
+    } else {
+      // Ask to enter state to complete form submission
+      handleFieldChange(FormFields.State, {
+        error: true,
+        errorMsg: `The above fields are valid! Please input data into this field.`,
+      });
+      return null;
     }
-
-    return validation;
   };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    // Reset errors
+    handleFieldChange(FormFields.Postcode, {});
+    handleFieldChange(FormFields.Suburb, {});
+    handleFieldChange(FormFields.State, {});
 
     const isValid = handleLocalValidation();
     if (!isValid) return;
@@ -155,20 +205,20 @@ export default function Home() {
           onChange={(value) => handleFieldChange(FormFields.Postcode, { value })}
         />
         <InputField
-          name={FormFields.State}
-          label='State'
-          error={formState[FormFields.State].error}
-          errorMsg={formState[FormFields.State].errorMsg}
-          value={formState[FormFields.State].value}
-          onChange={(value) => handleFieldChange(FormFields.State, { value })}
-        />
-        <InputField
           name={FormFields.Suburb}
           label='Suburb'
           error={formState[FormFields.Suburb].error}
           errorMsg={formState[FormFields.Suburb].errorMsg}
           value={formState[FormFields.Suburb].value}
           onChange={(value) => handleFieldChange(FormFields.Suburb, { value })}
+        />
+        <InputField
+          name={FormFields.State}
+          label='State'
+          error={formState[FormFields.State].error}
+          errorMsg={formState[FormFields.State].errorMsg}
+          value={formState[FormFields.State].value}
+          onChange={(value) => handleFieldChange(FormFields.State, { value })}
         />
         <button className='btn-primary' type='submit' disabled={loading}>
           {loading ? 'Loading...' : 'Validate'}
